9b323af3dc007954f9a92444f61b3221b09a473a
Faruk <97340877+Faruk-Tutkus@users.noreply.github.com>
2023-10-30 12:18:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ window.onload = (event) => {
     meaningWord.innerHTML = filteredWords[keysNum].turkish
 }
 buttonright.addEventListener('click', ()=> {
-    queue++;
+    queue = Math.random() * 1000
     keysNum = keys[queue]
     englishWord.innerHTML = filteredWords[keysNum].english
     meaningWord.innerHTML = filteredWords[keysNum].turkish
